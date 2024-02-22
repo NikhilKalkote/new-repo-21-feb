8fac9910794ee1ddf383b5ea28625c561f4e5c70
NikhilKalkote <143207384+NikhilKalkote@users.noreply.github.com>
2024-02-22 10:22:48 +0530

    Commit after adding Webhook on GITHub

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from AWS and Jenkins SCM polling and webhook!" );
+        System.out.println( "Hello World from AWS and Jenkins SCM polling and Webhook!" );
     }
 }
